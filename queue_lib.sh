c8a1b04b2c571590e95dd300aee5022055beb3eb
felipecr <cruz@cscs.ch>
2024-11-15 02:26:56 +0100

    push to file based entries

--- queue_lib.sh
+++ queue_lib.sh
@@ -1,5 +1,4 @@
-QUEUE_FILE="/tmp/exec_queue"
-QUEUE_LOCK="/tmp/exec_queue.lock"
+QUEUE_DIR="/tmp/exec_queue"
 EXEC_LOCK="/tmp/exec_lock"
 
 # Debug messages only if DEBUG_MODE is on
@@ -14,11 +13,9 @@ register() {
   local timestamp="$2"
   local pid="$3"
 
-  # Atomic write into queue
-  (
-    flock -x 200
-    echo "$priority $timestamp $pid" >> "$QUEUE_FILE"
-  ) 200>"$QUEUE_LOCK"
+
+  mkdir -p $QUEUE_DIR/$pid
+  echo "$priority $timestamp $pid" > "$QUEUE_DIR/$pid/task_info"
 }
 
 wait_turn() {
@@ -27,19 +24,14 @@ wait_turn() {
   local pid="$3"
 
   while true; do
-    local first_entry=$(
-      (
-        flock -s 200
-        sort -k1,1n -k2,2n "$QUEUE_FILE" | head -n1
-      ) 200>"$QUEUE_LOCK"
-    )
+    local first_task=$(find "$QUEUE_DIR" -type d -exec bash -c 'cat "$0/task_info"' {} \; | sort -k1,1n -k2,2n | head -n 1)
 
-    # Debug output
     debug_echo "Checking if current task is first in queue..."
     debug_echo "Expected entry: '$priority $timestamp $pid'"
-    debug_echo "First entry in queue: '$first_entry'"
+    debug_echo "First entry in queue: '$first_task'"
 
-    if [[ "$first_entry" == "$priority $timestamp $pid" ]]; then
+    # Check if the current task matches the first sorted task entry
+    if [[ "$first_task" == "$priority $timestamp $pid" ]]; then
       debug_echo "This task is first in the queue. Proceeding..."
       break
     else
@@ -50,19 +42,14 @@ wait_turn() {
 }
 
 start_exe() {
-  # extra lock for critical path
+  # Critical path
   exec 300>"$EXEC_LOCK"
   flock -x 300
 }
 
 finish_exe() {
-  # Finished and remove itself from queue
-  (
-    flock -x 200
-    sed -i "/^$1 $2 $3$/d" "$QUEUE_FILE"
-  ) 200>"$QUEUE_LOCK"
+  rm -rf "$QUEUE_DIR/$1"
 
   flock -u 300
   exec 300>&-
 }
-
